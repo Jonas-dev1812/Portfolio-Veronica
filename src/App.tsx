f05fa49ba8955c8571892bf6ee5068afd088acc7
Jonas-dev1812 <jonas.hilberink@gmail.com>
2025-07-17 21:54:04 +0200

    Add Analyitics for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import AboutMe from "./components/AboutMe/AboutMe";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <AboutMe />
+      <Analytics />
     </>
   );
 }
