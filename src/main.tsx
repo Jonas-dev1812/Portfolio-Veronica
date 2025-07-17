27b5d4eea54ecbeea94e48a62ae49d46cbbbd15e
Jonas-dev1812 <jonas.hilberink@gmail.com>
2025-07-17 21:48:02 +0200

    add SpeedInsights for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx"; // Dein benutzerdefinierter Provider (ChakraProvider + ColorModeProvider)
 import { LanguageProvider } from "./contexts/LanguageContext.tsx"; // LanguageProvider importieren
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <LanguageProvider>
+        <SpeedInsights />
         <App />
       </LanguageProvider>
     </Provider>
